f50117d1bfc8b8281cf752f475117d8371473e39
FrancescoPepi <francescopepi16@gmail.com>
2023-10-02 20:08:09 +0200

    add php in html

--- BackEnd/main.php
+++ BackEnd/main.php
@@ -1,27 +1,23 @@
 <?php
 
-include './Models/Genre.php';
-include './Models/Movie.php';
-include './Database/db.php';
+include __DIR__ . './Models/Genre.php';
+include __DIR__ . './Models/Movie.php';
+include __DIR__ . './Database/db.php';
 $movies = [];
 foreach ($movies_data as $movie_data) {
-    $genres = [];
-    foreach ($movie_data['genres'] as $genre_data) {
-        $genres[] = new Genre($genre_data);
+  $genres = [];
+  foreach ($movie_data['genres'] as $genre_data) {
+    $genres[] = new Genre($genre_data);
 
-    }
-    // echo "<pre>";
-    // var_dump($genres);
-    // echo "</pre>";
-    $movies[] = new Movie($movie_data['title'], $genres, $movie_data['data_out'], $movie_data['vote']);
-
+  }
+  $movies[] = new Movie($movie_data['title'], $genres, $movie_data['data_out'], $movie_data['vote']);
 }
 ;
-foreach ($movies as $movie) {
-    echo $movie->get_film();
-    echo '<hr>';
-}
-;
+// foreach ($movies as $movie) {
+//     echo $movie->get_film();
+//     echo '<hr>';
+// }
+// ;
 // echo "<pre>";
 // var_dump($movies);
 // echo "</pre>";
--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -1,13 +1,34 @@
+<?php
+
+include __DIR__ . '../../BackEnd/main.php'
+
+  ?>
+
 <!DOCTYPE html>
 <html lang="it">
 
 <head>
-    <?php included __DIR__ . '/ComponentHtml/head.php' ?>
+  <?php include __DIR__ . '/ComponentHtml/head.php' ?>
 </head>
 
 <body>
-    <div id="app">{{ message }}</div>
+  <!-- <div id="app">{{ message }}</div> -->
+  <div class="container mt-5">
+    <div class="row">
+      <?php foreach ($movies as $movie): ?>
 
+        <div class="col">
+          <div class="card p-3">
+            <div class="card-text text-center">
+              <?php echo $movie->get_film(); ?>
+
+            </div>
+          </div>
+        </div>
+      <?php endforeach; ?>
+
+    </div>
+  </div>
 </body>
 
 </html>
